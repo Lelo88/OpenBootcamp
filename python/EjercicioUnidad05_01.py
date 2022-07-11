30f16031a797d2709d3059f3f6970240f2dcfa1f
Leandro Villalba <63604403+Lelo88@users.noreply.github.com>
2022-07-11 13:25:16 -0300

    Create EjercicioUnidad05_01.py

---
+++ python/EjercicioUnidad05_01.py
@@ -0,0 +1,13 @@
+def areaTriangulo(base, altura):
+    area = 0
+    area = (base*altura)/2
+    print(area)
+
+areaTriangulo(4,9)
+
+def areaCirculo(radio):
+    area=0
+    area = 3.1416 * radio**2
+    print(area)
+
+areaCirculo(2)
