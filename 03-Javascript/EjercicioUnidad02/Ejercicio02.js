ba2439f50a4aab28fb756f6308748564084a1fef
Leandro Villalba <63604403+Lelo88@users.noreply.github.com>
2022-08-05 09:55:42 -0300

    Create Ejercicio02.js

---
+++ 03-Javascript/EjercicioUnidad02/Ejercicio02.js
@@ -0,0 +1,20 @@
+//nombre
+let nombreCompleto = "Leandro Gabriel Villalba";
+
+//edad
+let edad = 34;
+
+//¿Eres desarrollador?
+let desarrollador = True;
+
+//fecha de nacimiento
+
+//30 de jullio de 1988
+const fechaNac=new Date(1988,6,30)
+
+const libro = {
+    titulo: "Las aventuras de Sherlock Holmes",
+    autor: "Arthur Conan Doyle",
+    fecha: new Date(1896,5,24),
+    url: null
+}
