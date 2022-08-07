81c5d6761351aa933487a9815db0195b66d9c45e
Leandro Villalba <63604403+Lelo88@users.noreply.github.com>
2022-08-07 00:15:10 -0300

    Add files via upload

---
+++ 03-Javascript/Ejercicio04.js
@@ -0,0 +1,16 @@
+let myName = "Leandro"; 
+let mySurname = "Villalba";
+
+let student = `${myName} ${mySurname}`;
+
+let studentMayus = student.toUpperCase();
+let studentMinus = student.toLowerCase();
+
+let studentLenght = student.length;
+
+let letterName = student[0];
+let lastLetterName = student[23]
+
+let nameWithoutSpace = student.replace(" ","");
+
+let studentName = student.includes(myName)
