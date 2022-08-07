050aa8329427335a81ca654cd75cc7cf238ea666
Leandro Villalba <63604403+Lelo88@users.noreply.github.com>
2022-08-07 12:04:13 -0300

    Add files via upload

---
+++ 03-Javascript/Ejercicio05.js
@@ -0,0 +1,13 @@
+let alturaCms = 185;
+
+let alturaMts = 1.85;
+
+let pesoKgs = 105.8;
+
+let alturaRed = Math.round(alturaMts);
+
+let pesoRed = Math.floor(pesoKgs)
+
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
+
