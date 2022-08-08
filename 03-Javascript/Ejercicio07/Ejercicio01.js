9ff6ecc2b6ae24c0fe7b5e2de03c49a892f5c8e0
Leandro Villalba <63604403+Lelo88@users.noreply.github.com>
2022-08-08 10:09:43 -0300

    Create Ejercicio01.js

---
+++ 03-Javascript/Ejercicio07/Ejercicio01.js
@@ -0,0 +1,9 @@
+const familia = ['Luna', 'Romina', 'Leandro','Leandro']
+const setFamilia = new Set(familia)
+
+console.log(familia)
+console.log(setFamilia)
+
+setFamilia.add('Javascript')
+
+console.log(setFamilia)
