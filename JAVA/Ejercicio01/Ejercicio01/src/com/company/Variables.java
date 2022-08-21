8b09c686c056fbacaa57ac8db9b0597568e954ff
Leandro Villalba <63604403+Lelo88@users.noreply.github.com>
2022-08-21 16:28:28 -0300

    Add files via upload

---
+++ JAVA/Ejercicio01/Ejercicio01/src/com/company/Variables.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Variables {
+    public static void main(String[] args) {
+        int numInt = 10;
+        double numFloat= 1.55;
+        boolean booleano = true;
+        long numlong = 4564656;
+        String nombre = "Leandro Gabriel Villalba";
+
+        System.out.println(numInt);
+        System.out.println(numFloat);
+        System.out.println(booleano);
+        System.out.println(numlong);
+        System.out.println(nombre); 
+    }
+}
