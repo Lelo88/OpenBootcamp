b650832935fb1059e993911fef372f75424f3356
Leandro Villalba <63604403+Lelo88@users.noreply.github.com>
2022-08-21 17:25:50 -0300

    Add files via upload

---
+++ JAVA/Ejercicio03/src/com/company/Ejercicio03.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Ejercicio03 {
+    public static void main(String[] args) {
+
+        String[] nombre = new String[]{"Leandro", "Gabriel", "Villalba"};
+
+        //Recorremos el array y mostramos su contenido
+        for(int i = 0; i < nombre.length; i++) {
+            System.out.print(nombre[i] + " ");
+        }
+    }
+}
